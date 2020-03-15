7d22089949208c8f2959936e0dfac7b2ace02ec7
Saad Guessous <guessous_saad@hotmail.com>
2020-03-15 17:36:22 +0200

    fixed failing test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Corona!/i);
+  const linkElement = getByText(/Corona 2020!/i);
   expect(linkElement).toBeInTheDocument();
 });
